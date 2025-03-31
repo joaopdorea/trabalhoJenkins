8acfb1207879dc21ec813fcb368ca4ffc279fc1d
João Dorea <84815558+joaopdorea@users.noreply.github.com>
2025-03-31 19:50:02 -0300

    Add files via upload

---
+++ src/main/java/control/Calculadora.java
@@ -0,0 +1,20 @@
+package control;
+
+public class Calculadora {
+
+    public double somar (double param1, double param2) {
+        return param1 + param2;
+    }
+
+    public double subtrair (double param1, double param2) {
+        return param1 - param2;
+    }
+
+    public double multiplicar (double param1, double param2) {
+        return param1 * param2;
+    }
+
+    public double dividir (double param1, double param2) {
+        return param1 / param2;
+    }
+}
